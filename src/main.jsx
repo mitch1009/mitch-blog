916d3a5873d0a67df9fc4231ddb9abfe9529737a
Mitch chimwemwe Chanza <hello@mitch.guru>
2023-05-02 18:07:31 +0200

    Revert "changed the app component to my component"
    This reverts commit 56d0eba06a834b783b03f94c76e4fe8f359ae6c5.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {MitchComponent} from "./App"
+import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MitchComponent title={"Giberish"} description={"My Best Description"} />
+    <App />
   </React.StrictMode>,
 )
